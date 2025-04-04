62c7d60f4c585fd8150f069783fc492a29f14841
JairGZZ <jairprox1234>
2025-04-04 10:52:28 -0500

    Initial Commit

---
+++ FinalProyectDAM2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinalProyectDAM2
+//
+//  Created by DAMII on 4/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
